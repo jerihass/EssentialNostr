476a3fd952e583122a848ff465cf173274a87c56
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-05-16 19:54:55 -0400

    Add memory leak tracking to tests.

--- EssentialNostrTests/Event API/NetworkConnectionWebSocketClientTests.swift
+++ EssentialNostrTests/Event API/NetworkConnectionWebSocketClientTests.swift
@@ -33,15 +33,15 @@ class NetworkConnectionWebSocketClientTests: XCTestCase {
     }
 
     func test_receive_sendsRequestToServer() {
-        let sut = makeSUT()
+        let sut: NetworkConnectionWebSocketClient = makeSUT()
         var echo: Data?
         let request = "Request"
         let data = request.data(using: .utf8)!
 
         let exp = expectation(description: "Wait for ready")
 
-        sut.stateHandler = {
-            if $0 == .ready { sut.receive(with: request) }
+        sut.stateHandler = { [weak sut] in
+            if $0 == .ready { sut?.receive(with: request) }
         }
 
         sut.receiveHandler = {
@@ -57,10 +57,10 @@ class NetworkConnectionWebSocketClientTests: XCTestCase {
     }
 
     // MARK: - Helpers
-    private func makeSUT() -> NetworkConnectionWebSocketClient {
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> NetworkConnectionWebSocketClient {
         let url = URL(string: "wss://127.0.0.1:8080")!
         let sut = NetworkConnectionWebSocketClient(url: url)
-
+        trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
 }
