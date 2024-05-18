f6463f3f39f3bd937c825a40f2e740901bb46fae
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-05-18 12:19:38 -0400

    Update deprication messages. stateHandler is part of delegate, receiveHandler is just receive with completion block.

--- EssentialNostr/Event API/WebSocketClient.swift
+++ EssentialNostr/Event API/WebSocketClient.swift
@@ -6,23 +6,24 @@ import Foundation
 import Network
 
 public protocol WebSocketDelegate: AnyObject {
-    func send(message: String, completion: @escaping (Error) -> Void)
-    func receive(completion: @escaping (_ result: Result<Data, Error>) -> Void)
+    var stateHandler: ((_ state: NWConnection.State) -> Void)? { get set }
 }
 
 public protocol WebSocketClient: AnyObject {
     typealias ReceiveResult = Result<Data, Error>
     var delegate: WebSocketDelegate? { get set }
-    @available(*, deprecated, renamed: "WebSocketDelegate.stateHandler", message: "Use WebSocketDelegate instead")
+
+    @available(*, deprecated, renamed: "delegate.stateHandler", message: "Use WebSocketDelegate instead")
     var stateHandler: ((_ state: NWConnection.State) -> Void)? { get set }
 
-    @available(*, deprecated, renamed: "WebSocketDelegate.receiveHandler", message: "Use WebSocketDelegate instead")
+    @available(*, deprecated, renamed: "receive", message: "Use WebSocketClient.receive instead")
     var receiveHandler: ((_ result: ReceiveResult) -> Void)? { get set }
 
-    @available(*, deprecated, renamed: "send", message: "Use send / receive instead")
+    @available(*, deprecated, renamed: "send", message: "Use WebSocketClient.send instead")
     func receive(with request: String, completion: @escaping (ReceiveResult) -> Void)
 
     func send(message: String, completion: @escaping (Swift.Error) -> Void)
+    
     func receive(completion: @escaping (_ result: ReceiveResult) -> Void)
     func start() throws
     func disconnect()
