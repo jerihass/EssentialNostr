f5262480634caf85871be041837b0e75ef8c75e3
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-05-19 15:10:46 -0400

    Test sending a bad event to a relay, gives error.

--- EssentialNostrAPIEndToEndTests/EssentialNostrAPIEndToEndTests.swift
+++ EssentialNostrAPIEndToEndTests/EssentialNostrAPIEndToEndTests.swift
@@ -7,8 +7,8 @@ import EssentialNostr
 import Network
 
 final class EssentialNostrAPIEndToEndTests: XCTestCase {
+    // Use this test on local echo server modified for event canned responses
     func test_endToEndTestServer_retrievesExpectedEvents() throws {
-
         let url = URL(string: "ws://127.0.0.1:8080")!
         let client = NetworkConnectionWebSocketClient(url: url)
         let delegate = Delegate()
@@ -37,8 +37,42 @@ final class EssentialNostrAPIEndToEndTests: XCTestCase {
         }
     }
 
+    // use this test on valid nostr server
+    func test_endToEndTestServer_badEventJSONGivesErrorReply() throws {
+        let url = URL(string: "ws://127.0.0.1:8080")!
+        let client = NetworkConnectionWebSocketClient(url: url)
+        let delegate = Delegate()
+        delegate.stateHandler = { _ in }
+        client.delegate = delegate
+        let loader = RemoteEventLoader(client: client)
+
+        try client.start()
+
+        let event = Event(id: "bdID", pubkey: "npub1el277q4kesp8vhs7rq6qkwnhpxfp345u7tnuxykwr67d9wg0wvyslam5n0", created_at: .now, kind: 1, tags: [], content: "Test", sig: "badsig")
+        let message = ClientMessage.Message.event(event: event)
+
+        loader.request(ClientMessageMapper.mapMessage(message))
+        var receivedResult: LoadEventResult?
+
+        let exp = expectation(description: "Wait for load completion")
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 3.0)
+
+        switch receivedResult {
+        case let .success(event)?:
+            XCTFail("Expected failure, got \(event) instead.")
+        case let .failure(error)?:
+            print(error)
+            XCTAssertNotNil(error, "Expected Error")
+        default:
+            XCTFail("Expected successful event result, got no result instead.")
+        }
+    }
+
     class Delegate: WebSocketDelegate {
         var stateHandler: ((NWConnection.State) -> Void)?
-
     }
 }
