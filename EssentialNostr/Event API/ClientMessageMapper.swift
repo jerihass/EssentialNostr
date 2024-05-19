4245f26259e0702bada4b830259b1e6b0bf99dea
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-05-19 12:08:18 -0400

    Mapping filters with since, until and limit.

--- EssentialNostr/Event API/ClientMessageMapper.swift
+++ EssentialNostr/Event API/ClientMessageMapper.swift
@@ -50,7 +50,8 @@ public final class ClientMessageMapper {
                 return "[\"EVENT\",\(string)]"
             }
         case let .request(sub, filters):
-            if let filterJSON = filters.json, let string = String(data: filterJSON, encoding: .utf8) {
+            let local = filters.map(MessageFilter.init)
+            if let filterJSON = local.json, let string = String(data: filterJSON, encoding: .utf8) {
                 var trimmed = string
                 trimmed = trimmed.trimmingCharacters(in: ["[","]"])
                 return "[\"REQ\",\"\(sub)\",\(trimmed)]"
@@ -82,5 +83,63 @@ public final class ClientMessageMapper {
             let encoder = JSONEncoder()
             return try? encoder.encode(self)
         }
+    }
+}
+
+private struct MessageFilter: Encodable {
+    let ids: [String]?
+    let authors: [String]?
+    let kinds: [UInt16]?
+    let tags: [[String]]?
+    let since: Double?
+    let until: Double?
+    let limit: UInt?
+
+    init(ids: [String]? = nil, authors: [String]? = nil, kinds: [UInt16]? = nil, tags: [[String]]? = nil, since: Double? = nil, until: Double? = nil, limit: UInt? = nil) {
+        self.ids = ids
+        self.authors = authors
+        self.kinds = kinds
+        self.tags = tags
+        self.since = since
+        self.until = until
+        self.limit = limit
+    }
+
+    enum CodingKeys: CodingKey {
+        case ids
+        case authors
+        case kinds
+        case tags
+        case since
+        case until
+        case limit
+    }
+
+    init(_ filter: Filter) {
+        ids = filter.ids
+        authors = filter.authors
+        kinds = filter.kinds
+        tags = filter.tags
+        since = filter.since?.timeIntervalSince1970
+        until = filter.until?.timeIntervalSince1970
+        limit = filter.limit
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container: KeyedEncodingContainer<MessageFilter.CodingKeys> = encoder.container(keyedBy: MessageFilter.CodingKeys.self)
+        try container.encodeIfPresent(self.ids, forKey: MessageFilter.CodingKeys.ids)
+        try container.encodeIfPresent(self.authors, forKey: MessageFilter.CodingKeys.authors)
+        try container.encodeIfPresent(self.kinds, forKey: MessageFilter.CodingKeys.kinds)
+        try container.encodeIfPresent(self.tags, forKey: MessageFilter.CodingKeys.tags)
+        try container.encodeIfPresent(self.since, forKey: MessageFilter.CodingKeys.since)
+        try container.encodeIfPresent(self.until, forKey: MessageFilter.CodingKeys.until)
+        try container.encodeIfPresent(self.limit, forKey: MessageFilter.CodingKeys.limit)
+    }
+}
+
+extension Array where Element == MessageFilter {
+    var json: Data? {
+        let encoder = JSONEncoder()
+        return try? encoder.encode(self)
     }
 }
--- EssentialNostrTests/Message API/ClientMessageMapperTests.swift
+++ EssentialNostrTests/Message API/ClientMessageMapperTests.swift
@@ -54,6 +54,19 @@ class ClientMessageMapperTests: XCTestCase {
         print("Mapped:  " + mapped)
     }
 
+    func test_map_requestMessageToString_sinceUntilLimit() {
+        let sub = "sub1"
+        let since: Date = .distantPast
+        let until: Date = .now
+        let filters = [Filter(since: since, until: until, limit: 10)]
+        let request = "[\"REQ\",\"\(sub)\",{\"since\":\(since.timeIntervalSince1970),\"until\":\(until.timeIntervalSince1970),\"limit\":10}]"
+        let requestMessage = ClientMessage.Message.request(sub: sub, filters: filters)
+        let mapped = ClientMessageMapper.mapMessage(requestMessage)
+        XCTAssertTrue(areJSONEqual(mapped.data(using: .utf8)!, request.data(using: .utf8)!))
+        print("Request: " + request)
+        print("Mapped:  " + mapped)
+    }
+
     // MARK: - Helpers
 
     private func makeEvent(id: String, pubkey: String, created_at: Date, kind: UInt16, tags: [[String]], content: String, sig: String) -> (model: Event, data: Data) {
