4214d25d6a5a3b00adb7b0a8b53737bb443d3fb4
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-05-20 23:22:14 -0400

    Confirm can hash base event json with sha256.

--- EssentialNostrTests/Keys/KeypairTests.swift
+++ EssentialNostrTests/Keys/KeypairTests.swift
@@ -5,6 +5,14 @@
 import XCTest
 import secp256k1
 
+struct BaseEvent {
+    public let pubkey: String
+    public let created_at: Date
+    public let kind: UInt16
+    public let tags: [[String]]
+    public let content: String
+}
+
 class Keypair {
     let privateKey: secp256k1.Signing.PrivateKey
     let publicKeyData: Data
@@ -20,4 +28,19 @@ class KeypairTests: XCTestCase {
         XCTAssertEqual(key.privateKey.dataRepresentation.count, 32)
         XCTAssertEqual(key.publicKeyData.count, 32)
     }
+
+    func test_sha256_eventJSONdata() {
+        let data = baseEvent(pubkey: "badpubkey", created_at: .now, kind: 1, tags: [["e"]], content: "content")
+        let eventID = SHA256.hash(data: data)
+        XCTAssertEqual(eventID.bytes.count, 32)
+    }
+}
+
+private func baseEvent(pubkey: String, created_at: Date, kind: UInt16, tags: [[String]], content: String) -> Data {
+    let event = BaseEvent(pubkey: pubkey, created_at: created_at, kind: kind, tags: tags, content: content)
+    let time = Int(created_at.timeIntervalSince1970)
+    let tagString = tags.stringed
+
+    let eventJSON = "{\"pubkey\":\"\(pubkey)\",\"created_at\":\(time),\"kind\":\(kind),\"tags\":\(tagString),\"content\":\"\(content)\"}"
+    return Data(eventJSON.utf8)
 }
