3a8cc7c8dcbb7b5f23f87eb159f87cac7489e786
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-05-21 15:24:25 -0400

    Converting hex string back to Data.

--- EssentialNostrTests/Keys/DataHexEncodingTests.swift
+++ EssentialNostrTests/Keys/DataHexEncodingTests.swift
@@ -15,10 +15,33 @@ extension Data {
     }
 }
 
+extension String {
+    var bytes: Data? {
+        guard self.count % 2 == 0 else { return nil }
+        // check all values between 0-9a-f
+        var bytes = [UInt8]()
+        var index = self.startIndex
+            for _ in 0..<self.count / 2 {
+                let nextIndex = self.index(index, offsetBy: 2)
+                let byteString = self[index..<nextIndex]
+                guard let byte = UInt8(byteString, radix: 16) else { return nil }
+                bytes.append(byte)
+                index = nextIndex
+            }
+        return Data(bytes)
+    }
+}
+
 class DataHexEncodingTests: XCTestCase {
     func test_hex_converts8BitToHex() {
-        let data: Data = Data([10, 11, 12, 13, 14])
+        let data: Data = Data([255, 17, 0, 15, 255])
         let hex = data.hex
-        XCTAssertEqual(hex, "abcde")
+        XCTAssertEqual(hex, "ff11000fff")
+    }
+
+    func test_data_convertsHexToBytes() {
+        let hex = "ff11000fff"
+        let out = hex.bytes
+        XCTAssertEqual(out, Data([255, 17, 0, 15, 255]))
     }
 }
