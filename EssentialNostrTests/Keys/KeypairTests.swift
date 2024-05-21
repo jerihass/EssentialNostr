5a33c144222bac32659e4b4289f0b59f6b3b62bd
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-05-20 23:05:56 -0400

    Confiirm that public key data produced by secp256k1 is 32 bytes if using xonly part of curve.

--- EssentialNostrTests/Keys/KeypairTests.swift
+++ EssentialNostrTests/Keys/KeypairTests.swift
@@ -7,9 +7,10 @@ import secp256k1
 
 class Keypair {
     let privateKey: secp256k1.Signing.PrivateKey
-
+    let publicKeyData: Data
     init() throws {
         privateKey = try secp256k1.Signing.PrivateKey()
+        publicKeyData = Data(privateKey.publicKey.xonly.bytes)
     }
 }
 
@@ -17,5 +18,6 @@ class KeypairTests: XCTestCase {
     func test_init_generatesPrivateKey() throws {
         let key = try Keypair()
         XCTAssertEqual(key.privateKey.dataRepresentation.count, 32)
+        XCTAssertEqual(key.publicKeyData.count, 32)
     }
 }
