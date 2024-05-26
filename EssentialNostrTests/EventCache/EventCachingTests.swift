f0d4eba935a9a83888459424672c9441c98c8593
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-05-26 15:42:50 -0400

    Create SUT helper method.

--- EssentialNostrTests/EventCache/EventCachingTests.swift
+++ EssentialNostrTests/EventCache/EventCachingTests.swift
@@ -25,20 +25,26 @@ class EventStore {
 
 class EventCachingTests: XCTestCase {
     func test_init_doesNoteDeleteCacheWhenCreated() {
-        let store = EventStore()
-        _ = LocalEventLoader(store: store)
+        let (_, store) = makeSUT()
         XCTAssertEqual(store.deleteCachedEventsCallCount, 0)
     }
 
     func test_save_requestsCacheDeletion() {
-        let store = EventStore()
-        let sut = LocalEventLoader(store: store)
+        let (sut, store) = makeSUT()
         let events = [uniqueEvent(), uniqueEvent()]
         sut.save(events)
         XCTAssertEqual(store.deleteCachedEventsCallCount, 1)
     }
 
     // MARK: - Helpers
+
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalEventLoader, store: EventStore) {
+        let store = EventStore()
+        let sut = LocalEventLoader(store: store)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(store, file: file, line: line)
+        return (sut, store)
+    }
 
     private func uniqueEvent() -> Event {
         return Event(id: UUID().uuidString, pubkey: "pubkey", created_at: .now, kind: 1, tags: [[]], content: "Some content", sig: "signature")
