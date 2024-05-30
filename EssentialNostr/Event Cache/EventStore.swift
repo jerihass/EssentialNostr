e161eedd49ff5c18eb032a84144055869a8be7f8
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-05-29 20:58:43 -0400

    Move LocalEvent to own file.

--- EssentialNostr/Event Cache/EventStore.swift
+++ EssentialNostr/Event Cache/EventStore.swift
@@ -10,23 +10,3 @@ public protocol EventStore {
     func deleteCachedEvents(completion: @escaping DeletionCompletion)
     func insert(_ events: [LocalEvent], completion: @escaping InsertionCompletion)
 }
-
-public struct LocalEvent: Equatable {
-    public let id: String
-    public let pubkey: String
-    public let created_at: Date
-    public let kind: UInt16
-    public let tags: [[String]]
-    public let content: String
-    public let sig: String
-
-    public init(id: String, pubkey: String, created_at: Date, kind: UInt16, tags: [[String]], content: String, sig: String) {
-        self.id = id
-        self.pubkey = pubkey
-        self.created_at = created_at
-        self.kind = kind
-        self.tags = tags
-        self.content = content
-        self.sig = sig
-    }
-}
---
+++ EssentialNostr/Event Cache/LocalEvent.swift
@@ -0,0 +1,25 @@
+//
+//  Created by Jericho Hasselbush on 5/29/24.
+//
+
+import Foundation
+
+public struct LocalEvent: Equatable {
+    public let id: String
+    public let pubkey: String
+    public let created_at: Date
+    public let kind: UInt16
+    public let tags: [[String]]
+    public let content: String
+    public let sig: String
+
+    public init(id: String, pubkey: String, created_at: Date, kind: UInt16, tags: [[String]], content: String, sig: String) {
+        self.id = id
+        self.pubkey = pubkey
+        self.created_at = created_at
+        self.kind = kind
+        self.tags = tags
+        self.content = content
+        self.sig = sig
+    }
+}
