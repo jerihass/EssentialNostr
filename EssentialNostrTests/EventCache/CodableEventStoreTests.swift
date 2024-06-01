34689227874f3e573c056ce1c9b268be9f75feff
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-05-31 22:45:54 -0400

    Prove taht codable feed store tests run serially.

--- EssentialNostrTests/EventCache/CodableEventStoreTests.swift
+++ EssentialNostrTests/EventCache/CodableEventStoreTests.swift
@@ -119,6 +119,32 @@ class CodableEventStoreTests: XCTestCase {
         XCTAssertNotNil(deletionError, "Exepected cache deletion to fail")
     }
 
+    func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
+
+        var completedOps = [XCTestExpectation]()
+        let op1 = expectation(description: "Operation 1")
+        sut.insert(uniqueEvents().local) { _ in
+            completedOps.append(op1)
+            op1.fulfill()
+        }
+
+        let op2 = expectation(description: "Operation 2")
+        sut.deleteCachedEvents { _ in
+            completedOps.append(op2)
+            op2.fulfill()
+        }
+
+        let op3 = expectation(description: "Operation 3")
+        sut.insert(uniqueEvents().local) { _ in
+            completedOps.append(op3)
+            op3.fulfill()
+        }
+
+        wait(for: [op1, op2, op3], enforceOrder: true)
+        XCTAssertEqual(completedOps, [op1, op2, op3])
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #file, line: UInt = #line) -> EventStore {
