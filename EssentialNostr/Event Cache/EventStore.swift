daa92d378be69f10dfb9e1431f94f4f08256f8ee
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-05-31 23:09:48 -0400

    Add comments to document completions blocks can be invoked in any thread - client's are responsible.

--- EssentialNostr/Event Cache/EventStore.swift
+++ EssentialNostr/Event Cache/EventStore.swift
@@ -8,7 +8,16 @@ public protocol EventStore {
     typealias InsertionCompletion = (Error?) -> Void
     typealias DeletionCompletion = (Error?) -> Void
     typealias RetrievalCompletion = (Result<[LocalEvent], Error>) -> Void
+
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads if needed.
     func deleteCachedEvents(completion: @escaping DeletionCompletion)
+
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads if needed.
     func insert(_ events: [LocalEvent], completion: @escaping InsertionCompletion)
+
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads if needed.
     func retrieve(completion: @escaping RetrievalCompletion)
 }
