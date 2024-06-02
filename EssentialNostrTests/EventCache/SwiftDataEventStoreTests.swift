1b99bb0300832c3d01c9816d46b7853e162e6dd6
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-06-01 20:13:05 -0400

    Extract sut into helper factory method and implement in-store memory for testing only. And ensure inserved events can be retrieved.

--- EssentialNostrTests/EventCache/SwiftDataEventStoreTests.swift
+++ EssentialNostrTests/EventCache/SwiftDataEventStoreTests.swift
@@ -8,36 +8,62 @@ import EssentialNostr
 
 @available(macOS 14, *)
 class SwiftDataEventStore: EventStore {
+    private let container: ModelContainer
+
+    init(container: ModelContainer) {
+        self.container = container
+    }
+
     func deleteCachedEvents(completion: @escaping DeletionCompletion) {
 
     }
     
-    func insert(_ events: [EssentialNostr.LocalEvent], completion: @escaping InsertionCompletion) {
-
+    @MainActor func insert(_ events: [EssentialNostr.LocalEvent], completion: @escaping InsertionCompletion) {
+        let sdEvents = events.toSwiftData()
+        for event in sdEvents { container.mainContext.insert(event) }
+        completion(nil)
     }
     
-    func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.success([]))
+    @MainActor func retrieve(completion: @escaping RetrievalCompletion) {
+        let sdEvents = FetchDescriptor<SDEvent>()
+        do {
+            let found = try container.mainContext.fetch(sdEvents).map(\.local)
+            completion(.success(found))
+        } catch {
+            completion(.success([]))
+        }
+    }
+}
+
+@Model
+private class SDEvent {
+    public let id: String
+    public let publickey: String
+    public let created: Date
+    public let kind: UInt16
+    public let tags: [[String]]
+    public let content: String
+    public let signature: String
+
+    public init(id: String, publicKey: String, created: Date, kind: UInt16, tags: [[String]], content: String, signature: String) {
+        self.id = id
+        self.publickey = publicKey
+        self.created = created
+        self.kind = kind
+        self.tags = tags
+        self.content = content
+        self.signature = signature
     }
 
-    @Model
-    class SDEvent {
-        public let id: String
-        public let publickey: String
-        public let created: Date
-        public let kind: UInt16
-        public let tags: [[String]]
-        public let content: String
-        public let signature: String
+    var local: LocalEvent {
+        LocalEvent(id: id, publicKey: publickey, created: created, kind: kind, tags: tags, content: content, signature: signature)
+    }
+}
 
-        public init(id: String, publicKey: String, created: Date, kind: UInt16, tags: [[String]], content: String, signature: String) {
-            self.id = id
-            self.publickey = publicKey
-            self.created = created
-            self.kind = kind
-            self.tags = tags
-            self.content = content
-            self.signature = signature
+extension Array where Element == LocalEvent {
+    fileprivate func toSwiftData() -> [SDEvent] {
+        map {
+            SDEvent(id: $0.id, publicKey: $0.publickey, created: $0.created, kind: $0.kind, tags: $0.tags, content: $0.content, signature: $0.signature)
         }
     }
 }
@@ -45,17 +71,18 @@ class SwiftDataEventStore: EventStore {
 @available(macOS 14, *)
 class SwiftDataEventStoreTests: XCTestCase, EventStoreSpecs {
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = SwiftDataEventStore()
+        let sut = makeSUT()
         assertThatStoreDeliversEmptyOnEmptyCache(sut)
     }
     
     func test_retrieve_hasNoSideEffects() {
-        let sut = SwiftDataEventStore()
+        let sut = makeSUT()
         assertThatRetrieveHasNoSideEffects(sut)
     }
     
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
-
+        let sut = makeSUT()
+        assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
@@ -92,5 +119,15 @@ class SwiftDataEventStoreTests: XCTestCase, EventStoreSpecs {
     
     func test_storeSideEffects_runSerially() {
 
+    }
+
+    // MARK: - Helpers
+
+    func makeSUT() -> SwiftDataEventStore {
+        let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try! ModelContainer(for: SDEvent.self, configurations: configuration)
+        let sut = SwiftDataEventStore(container: container)
+        trackForMemoryLeaks(sut)
+        return sut
     }
 }
