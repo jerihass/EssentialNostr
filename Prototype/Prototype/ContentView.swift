40ec0a6c7a8e536149502e418edc35eba7205d42
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-06-02 21:15:04 -0400

    Add empty project for prototype.

---
+++ Prototype/Prototype/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Prototype
+//
+//  Created by Jericho Hasselbush on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Prototype/Prototype/PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeApp.swift
+//  Prototype
+//
+//  Created by Jericho Hasselbush on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
