691d5fa173eaa0d9d7147227c77f80ce71ec00eb
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-06-05 21:24:04 -0400

    Add prototype SwiftUIView with example event UI.

--- Prototype/Prototype/ContentView.swift
+++ Prototype/Prototype/ContentView.swift
@@ -1,7 +1,4 @@
 //
-//  ContentView.swift
-//  Prototype
-//
 //  Created by Jericho Hasselbush on 6/2/24.
 //
 
@@ -9,7 +6,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        Text("Nostr Events")
+            .font(.title)
+        List {
+            noteView()
+            noteView()
+            noteView()
+            noteView()
+            noteView()
+            noteView()
+            noteView()
+            noteView()
+            noteView()
+            noteView()
+        }
+        .listStyle(.plain)
+    }
+
+    private func noteView() -> some View {
+        return Group {
+            HStack {
+                ZStack {
+                    RoundedRectangle(cornerSize: .init(width: 8.0, height: 8.0), style: .continuous)
+                        .frame(width: 48, height: 48)
+                        .foregroundStyle(.ultraThinMaterial)
+                    Image(systemName: "person.fill")
+                        .frame(width: 48, height: 48)
+                }
+                Text("npubsadasdasdasdasdasdasd")
+                    .lineLimit(1, reservesSpace: false)
+                Spacer()
+                Text("1303 5/31/24")
+            }
+            Text("Content content content Content content content Content content content Content content content Content content content Content content content Content content content Content content content ")
         }
     }
 }
