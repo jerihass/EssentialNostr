d5e55d380f84a9eff9476cd308ad3cc9f8684741
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-06-06 21:14:50 -0400

    Load call request load request from loader.

--- EssentialNostriOSTests/SwiftUI/EventsViewViewModelTests.swift
+++ EssentialNostriOSTests/SwiftUI/EventsViewViewModelTests.swift
@@ -6,20 +6,37 @@ import XCTest
 import EssentialNostr
 
 class EventsViewModel {
+    private let loader: EventsViewViewModelTests.LoaderSpy
     init(loader: EventsViewViewModelTests.LoaderSpy) {
-        
+        self.loader = loader
+    }
+
+    func loadEvents() {
+        loader.load()
     }
 }
 
 class EventsViewViewModelTests: XCTestCase {
     func test_init_doesNotLoadEvents() {
         let loader = LoaderSpy()
-        let sut = EventsViewModel(loader: loader)
+        let _ = EventsViewModel(loader: loader)
         XCTAssertEqual(loader.loadCallCount, 0)
+    }
+
+    func test_load_requestLoadFromLoader() {
+        let loader = LoaderSpy()
+        let sut = EventsViewModel(loader: loader)
+
+        sut.loadEvents()
+
+        XCTAssertEqual(loader.loadCallCount, 1)
     }
 
     class LoaderSpy {
         private(set) var loadCallCount = 0
+        func load() {
+            loadCallCount += 1
+        }
     }
 }
 
