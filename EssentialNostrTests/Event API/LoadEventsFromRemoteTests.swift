9a524364b7c806e616ebd6c524db9526cbb8ff20
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-06-08 13:13:03 -0400

    Load request a load from the event loader.

--- EssentialNostrTests/Event API/LoadEventsFromRemoteTests.swift
+++ EssentialNostrTests/Event API/LoadEventsFromRemoteTests.swift
@@ -6,25 +6,49 @@ import XCTest
 import EssentialNostr
 
 class RemoteEventsLoader {
+    let eventLoader: EventLoader
 
+    init(eventLoader: EventLoader) {
+        self.eventLoader = eventLoader
+    }
+
+    func load() {
+        eventLoader.load { _ in }
+    }
 }
 
 class LoadEventsFromRemoteTests: XCTestCase {
     func test_init_doesNotRequestLoadWhenCreated() {
         let eventLoader = RemoteLoaderSpy()
-        let sut = RemoteEventsLoader()
-        
+        let sut = RemoteEventsLoader(eventLoader: eventLoader)
+
         XCTAssertEqual(eventLoader.receivedMessages, [])
     }
 
+    func test_load_requestsEvents() {
+        let eventLoader = RemoteLoaderSpy()
+        let sut = RemoteEventsLoader(eventLoader: eventLoader)
+
+        sut.load()
+
+        XCTAssertEqual(eventLoader.receivedMessages, [.loadEvents])
+    }
+
+
+    // MARK: - Helpers
+
+
     private class RemoteLoaderSpy: EventLoader {
-        var receivedMessages = [String]()
+        enum Message {
+            case loadEvents
+        }
+        var receivedMessages = [Message]()
         func request(_ message: String) {
 
         }
         
         func load(_ completion: @escaping (LoadEventResult) -> Void) {
-
+            receivedMessages.append(.loadEvents)
         }
     }
 }
