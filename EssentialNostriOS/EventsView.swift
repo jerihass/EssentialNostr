84c043648f237fd3e7ce9182be37e0a352197143
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-06-09 21:31:39 -0400

    Extract title view into injected property.

--- EssentialNostriOS/EventsView.swift
+++ EssentialNostriOS/EventsView.swift
@@ -5,20 +5,38 @@
 import SwiftUI
 import EssentialNostr
 
+public struct TitleViewModel {
+    let title: String
+}
+
+public struct TitleView: View {
+    private let model: TitleViewModel
+    
+    public init(model: TitleViewModel) {
+        self.model = model
+    }
+
+    public var body: some View {
+        Text(model.title)
+            .font(.title)
+    }
+}
+
 public struct EventsView: View {
     @State private var eventModels = [EventModel]()
     private let fetchEvents: () -> [EventModel]
-    private let errorView: () -> ErrorView
-    public init(fetchEvents: @escaping () -> [EventModel], errorView: @escaping () -> ErrorView) {
+    private let errorView: ErrorView
+    private let titleView: TitleView
+    public init(titleView: TitleView, errorView: ErrorView, fetchEvents: @escaping () -> [EventModel]) {
+        self.titleView = titleView
         self.fetchEvents = fetchEvents
         self.errorView = errorView
     }
 
     public var body: some View {
-        Text("Nostr Events")
-            .font(.title)
+        titleView
 
-        errorView()
+        errorView
 
         List(eventModels, id:\.id) { model in
             EventView(model: model)
@@ -42,9 +60,10 @@ public struct EventsView: View {
         Event(id: "eventID3", publicKey: "pubkey3", created: .now, kind: 1, tags: [], content: "contents some 3", signature: "sig3"),
         Event(id: "eventID4", publicKey: "pubkey4", created: .now, kind: 1, tags: [], content: "contents some 4", signature: "sig4")
     ]
+    let titleView = TitleView(model: .init(title: "Nostr Events"))
     let errorModel = ErrorViewModel(message: "Error")
     let errorView = ErrorView(model: errorModel)
-    return EventsView {
+    return EventsView(titleView: titleView, errorView: errorView) {
         events.map(EventModel.init)
-    } errorView: { errorView }
+    }
 }
