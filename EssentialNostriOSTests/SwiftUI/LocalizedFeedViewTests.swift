27730205284cd6c803583ab30a8c97e1461bd7de
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-06-15 22:30:18 -0400

    Create helper method to check for localized strings.

--- EssentialNostriOSTests/SwiftUI/LocalizedFeedViewTests.swift
+++ EssentialNostriOSTests/SwiftUI/LocalizedFeedViewTests.swift
@@ -8,10 +8,17 @@ import XCTest
 class LocalizedFeedViewTests: XCTestCase {
     func test_feedViewTitle() {
         let sut = TitleViewModel()
+
+        XCTAssertEqual(sut.title, localized("EVENT_VIEW_TITLE"))
+    }
+
+    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "EventsFeed"
         let bundle = Bundle(for: NostrLocalizedStrings.self)
-        let localizedKey = "EVENT_VIEW_TITLE"
-        let localizedTitle = bundle.localizedString(forKey: localizedKey, value: nil, table: "EventsFeed")
-        XCTAssertEqual(sut.title, localizedTitle)
-        XCTAssertNotEqual(localizedKey, localizedTitle, "Missing localized string for key: \(localizedKey)")
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table \(table)", file: file, line: line)
+        }
+        return value
     }
 }
