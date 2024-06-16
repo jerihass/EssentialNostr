45f1c9679cae2ba5982883f7a4bb46521f8157f2
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-06-16 17:04:56 -0400

    Create empty NostrApp

---
+++ NostrApp/NostrApp/NostrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NostrAppApp.swift
+//  NostrApp
+//
+//  Created by Jericho Hasselbush on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct NostrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
