290e92768bcbb23c79812b0bb0911c0d69628ede
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-06-18 12:24:28 -0400

    Adding a client to the pool puts it in the pool.

--- EssentialNostrTests/WebSocketInfrastructure/WebSocketPoolTests.swift
+++ EssentialNostrTests/WebSocketInfrastructure/WebSocketPoolTests.swift
@@ -3,17 +3,38 @@
 //
 
 import XCTest
+import EssentialNostr
 
 class WebSocketPool {
-
+    var pool = [WebSocketClient]()
+    func add(client: WebSocketClient) {
+        pool.append(client)
+    }
 }
 
 class WebSocketPoolTests: XCTestCase {
     func test_init_poolDoesNotSendMessagesToPool() {
-        let sut = WebSocketPool()
+        let _ = WebSocketPool()
         let pool = PoolSpy()
 
         XCTAssertTrue(pool.receivedMessages.isEmpty)
+    }
+
+    func test_add_addsClientToPool() {
+        let sut = WebSocketPool()
+        let client = ClientSpy()
+        sut.add(client: client)
+
+        XCTAssertEqual(sut.pool.count, 1)
+    }
+
+
+    private class ClientSpy: WebSocketClient {
+        var stateHandler: ((EssentialNostr.WebSocketDelegateState) -> Void)?
+        func start() throws {}
+        func disconnect() {}
+        func send(message: String, completion: @escaping (Error) -> Void) {}
+        func receive(completion: @escaping (ReceiveResult) -> Void) {}
     }
 
     private class PoolSpy {
