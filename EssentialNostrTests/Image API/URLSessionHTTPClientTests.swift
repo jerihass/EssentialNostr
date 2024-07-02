2df4d51edc7774aedcf52f47f9f30b3ce88dd0d4
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-07-02 17:12:27 -0400

    Remove unncessary tests.

--- EssentialNostrTests/Image API/URLSessionHTTPClientTests.swift
+++ EssentialNostrTests/Image API/URLSessionHTTPClientTests.swift
@@ -16,16 +16,6 @@ class URLSessionHTTPClient {
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
-    func test_getFromURL_createsDataTaskWithURL() {
-        let url = URL(string: "http://any-url.com")!
-        let session = URLSessionSpy()
-        let sut = URLSessionHTTPClient(session: session)
-
-        sut.get(from: url)
-
-        XCTAssertEqual(session.receivedURLs, [url])
-    }
-
     func test_getFromURL_resumesDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
         let session = URLSessionSpy()
@@ -43,21 +33,15 @@ class URLSessionHTTPClientTests: XCTestCase {
 
     // MARK: - Helpers
     private class URLSessionSpy: URLSession {
-        var receivedURLs = [URL]()
         private var stubs = [URL:URLSessionDataTask]()
 
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            receivedURLs.append(url)
-            return stubs[url] ?? FakeURLSessionDataTask()
+            return stubs[url] ?? URLSessionDataTaskSpy()
         }
 
         func stub(url: URL, task: URLSessionDataTask) {
             stubs[url] = task
         }
-    }
-
-    private class FakeURLSessionDataTask: URLSessionDataTask {
-        override func resume() {}
     }
 
     private class URLSessionDataTaskSpy: URLSessionDataTask {
