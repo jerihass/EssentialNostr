3380de39c583c711c843982119cd44b3ecd32ef3
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-07-04 12:35:28 -0400

    Add additional example test case for event ID creation.

--- EssentialNostrTests/Keys/KeypairTests.swift
+++ EssentialNostrTests/Keys/KeypairTests.swift
@@ -26,6 +26,19 @@ class KeypairTests: XCTestCase {
 
         XCTAssertEqual(event.id, "f97819289cf0bcfb727ded99dc2ebc04d60fe9e4be0097e84fce8d1cc7f252b7")
     }
+
+    func test_event_fromBaseEventFillsID2() {
+        let pub = "b154080cb49639bb079a6a53c1d98e7130eeab3c61aa95dd9e38f9e400027cc7"
+        let created = Date(timeIntervalSince1970: .init(integerLiteral: 1720110007))
+        let kind: UInt16 = 1
+        let tags: [[String]] = []
+        let content = "Zach Bryan has already done way enough for Country to be put in the Hall of Fame"
+        let base = BaseEvent(pubkey: pub, created_at: created, kind: kind, tags: tags, content: content)
+
+        let event = Event(base)
+
+        XCTAssertEqual(event.id, "4e9d274e0817aca5dbc0d9abcdafaf6abf73faf8109d76e09dab241d7685694c")
+    }
 }
 
 private func baseEventJSONData(pubkey: String, created_at: Date, kind: UInt16, tags: [[String]], content: String) -> Data {
