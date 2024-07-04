0869c8259e5e0525e22d40becbf0a6fd60e0d5b8
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-07-03 21:04:06 -0400

    Move URLSessionHTTPClient to it's own file in production.

--- EssentialNostrTests/Image API/URLSessionHTTPClientTests.swift
+++ EssentialNostrTests/Image API/URLSessionHTTPClientTests.swift
@@ -5,30 +5,7 @@
 import XCTest
 import EssentialNostr
 
-class URLSessionHTTPClient: HTTPClient {
-    private let session: URLSession
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-
-    struct UnexpectedValuesRepresentation: Error {}
-
-    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success((data, response)))
-            }
-            else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
-        }.resume()
-    }
-}
-
 class URLSessionHTTPClientTests: XCTestCase {
-
     override func setUp() {
         super.setUp()
         URLProtocolStub.startInteceptingRequests()
