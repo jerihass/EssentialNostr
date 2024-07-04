e193ce01f433158ca28198288066bb672d31a053
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-07-03 21:43:03 -0400

    Finish url loading when observing request to enusre requests are finished so that they don't live longer than they are supposed to.

--- EssentialNostrTests/Image API/URLSessionHTTPClientTests.swift
+++ EssentialNostrTests/Image API/URLSessionHTTPClientTests.swift
@@ -146,7 +146,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
 
         override class func canInit(with request: URLRequest) -> Bool {
-            requestObserver?(request)
             return true
         }
 
@@ -159,6 +158,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
 
         override func startLoading() {
+            if let observer = URLProtocolStub.requestObserver {
+                client?.urlProtocolDidFinishLoading(self)
+                return observer(request)
+            }
 
             if let data = URLProtocolStub.stub?.data {
                 client?.urlProtocol(self, didLoad: data)
