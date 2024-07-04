8be3dc7c70a92969515ee27480f400df2a04d97b
Jericho Hasselbush <jericho@sal-et-lucem.com>
2024-07-04 12:24:56 -0400

    Move Event id and BaseEvent extensions to own file.

--- EssentialNostrTests/Keys/KeypairTests.swift
+++ EssentialNostrTests/Keys/KeypairTests.swift
@@ -32,36 +32,3 @@ private func baseEventJSONData(pubkey: String, created_at: Date, kind: UInt16, t
     let event = BaseEvent(pubkey: pubkey, created_at: created_at, kind: kind, tags: tags, content: content)
     return try! JSONEncoder().encode(event)
 }
-
-extension Event {
-    init(_ base: BaseEvent) {
-        self.init(id: base.eventID!, pubkey: base.pubkey, created_at: base.created_at, kind: base.kind, tags: base.tags, content: base.content, sig: "SIGNATURE")
-    }
-}
-
-extension BaseEvent: Encodable {
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.unkeyedContainer()
-        try? container.encode(0)
-        try? container.encode(pubkey)
-        try? container.encode(created_at.timeIntervalSince1970)
-        try? container.encode(kind)
-        try? container.encode(tags)
-        try? container.encode(content)
-    }
-
-    var eventID: String? {
-        guard let serialized = serialized else { return nil }
-        let hash = CryptoKit.SHA256.hash(data: serialized)
-        return Data(hash.bytes).hex
-    }
-
-    private var serialized: Data? {
-        do {
-            let json = try JSONEncoder().encode(self)
-            return json
-        } catch {
-            return nil
-        }
-    }
-}
